27fff96a350311efdae4e2f554e9d613e9f78c98
Greg Smith <gregsmith446@gmail.com>
2019-06-03 11:53:04 -0400

    object oriented version of solution

---
+++ CarData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UberFareEstimator
+{
+    class CarData
+    {
+        double cost_per_minute;
+        double cost_per_mile;
+
+        // create object with sole purpose is to assign arguments to variables
+        public CarData(double costPerMinute, double costPerMile)
+        {
+            this.cost_per_minute = costPerMinute;
+            this.cost_per_mile = costPerMile;
+        }
+
+        // this object calculates & returns the fare
+        public double calculateFareEstimate(int ride_time, int ride_distance)
+        {
+            double fare = 0;
+
+            fare = (ride_time * cost_per_minute) + (ride_distance * cost_per_mile);
+
+            return fare;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,45 +6,27 @@ namespace UberFareEstimator
     {
         static void Main(string[] args)
         {
-            // make instance of class that contains our method
-            var estimator = new Estimator();
+            // test data
+            int ride_time = 30;
+            int ride_distance = 7;
+            double[] cost_per_minute = { 0.2, 0.35, 0.4, 0.45 };
+            double[] cost_per_mile = { 1.1, 1.8, 2.3, 3.5 };
 
-            double[] cost_per_minute = new double[4];
-            double[] cost_per_mile = new double[4];
+            // create array to hold output data
+            double[] estimatedFare = new double[cost_per_mile.Length];
 
-            cost_per_minute[0] = .2;
-            cost_per_minute[0] = .35;
-            cost_per_minute[0] = .4;
-            cost_per_minute[0] = .45;
-
-            cost_per_mile[0] = 1.1;
-            cost_per_mile[1] = 1.8;
-            cost_per_mile[2] = 2.3;
-            cost_per_mile[3] = 3.5;
-
-            // run method in estimator class
-            var test = estimator.fareEstimator(30, 7, cost_per_minute[], cost_per_mile[]);
-
-
-
-            Console.WriteLine(test);
-        }
-
-        public class Estimator
-        {
-            public double[] fareEstimator(int ride_time, int ride_distance, double[] cost_per_minute, double[] cost_per_mile)
+            // loop through test data
+            for (int i = 0; i < cost_per_mile.Length; i++)
             {
+                // create instance of car object that takes test data arrays as input
+                CarData testCar = new CarData(cost_per_minute[i], cost_per_mile[i]);
 
-                // initialize array
-                double[] answer = new double[cost_per_mile.Length];
-
-                // loop through array, doing the equation for each item in cost_per_minute & cost_per_mile
-                for (int i = 0; i < cost_per_mile.Length; i++)
-                {
-                    answer[i] = (ride_time * cost_per_minute[i]) + (ride_distance * cost_per_mile[i]);
-                }
-                return answer;
+                // perform calculation
+                estimatedFare[i] = (ride_time * cost_per_minute[i]) + (ride_distance * cost_per_mile[i]);
             }
+
+            // print the array of doubles together as one print statement using join() b/c without that console just prints System.double[]
+            Console.WriteLine(String.Join(" ", estimatedFare));
         }
     }
 }
